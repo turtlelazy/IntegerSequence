eef5f7e0020717b2b3cc7d58fc2c13c80b80796e
turtlelazy <ishraqmahid@gmail.com>
2020-12-07 17:21:34 -0500

    moved tester code

--- Range.java
+++ Range.java
@@ -28,15 +28,5 @@ public class Range implements IntegerSequence{
           return current+1;
       }
   }
-  public static void main(String[] args){
-      IntegerSequence r = new Range(10,15);
-    while(r.hasNext()){
-      System.out.print(r.next());
-      if( r.hasNext() ){       
-         System.out.print( ", " );
-      }
-    }
-    System.out.println();
-  }
   
 }
