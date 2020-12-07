78913841998fd5aeb19ec69579bcb611d6940ba5
turtlelazy <ishraqmahid@gmail.com>
2020-12-07 17:18:57 -0500

    Added code for Range

--- Range.java
+++ Range.java
@@ -1,12 +1,42 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
-  public Range(int start,  int end){  }
-  public void reset(){  }
-  public int length(){return 0;}
-  public boolean hasNext(){return false;}
+  public Range(int start,  int end){  
+      this.start = start;
+      this.end = end;
+      current = start;
+  }
+
+  public void reset(){  
+      current = start;
+  }
+
+  public int length(){
+      return end - start;
+    }
+
+  public boolean hasNext(){
+      return length() > end-current;
+}
 
   //@throws NoSuchElementException
-  public int next(){return 0;}
+  public int next(){
+      if (!hasNext()){
+          throw new NoSuchElementException();
+      }
+      else{
+          return current+1;
+      }
+  }
+  public static void main(String[] args){
+      IntegerSequence r = new Range(10,15);
+    while(r.hasNext()){
+      System.out.print(r.next());
+      if( r.hasNext() ){       
+         System.out.print( ", " );
+      }
+    }
+    System.out.println();
+  }
   
 }
